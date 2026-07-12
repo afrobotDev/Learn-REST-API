1c511cf8e39d0352c4f6103082cfe73735af5e34
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-12 23:24:38 +0300

    Update server to verify MySQL connection before starting

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersRouter from "./routes/users.js";
+import pool from "./config/db.js";
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,16 @@ app.use(express.json());
 
 app.use("/users", usersRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+pool
+  .getConnection()
+  .then((conn) => {
+    console.log("MySQL connected successfully");
+    conn.release();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MySQL:", err.message);
+    process.exit(1);
+  });
